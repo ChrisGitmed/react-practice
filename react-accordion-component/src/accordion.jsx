1ed697bcdd94dd9ec95d4c813cdc95e7fa1d3e48
Chris Gitmed <cgitmed@gmail.com>
2021-01-27 01:47:16 +0000

    Got basic functionality working.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const id = event.currentTarget.id;
+    if (this.state.text === id) {
+      this.setState({text: ''});
+    } else this.setState({ text: id })
+  }
+
   render() {
+    const { handleClick } = this;
+    const { text } = this.state;
+
+    let htmlTextClass = 'hidden';
+    let cssTextClass = 'hidden';
+    let jsTextClass = 'hidden';
+
+    if ( text === 'html') {
+      htmlTextClass = ''
+      cssTextClass = 'hidden'
+      jsTextClass = 'hidden'
+    } else if (text === 'css') {
+      htmlTextClass = 'hidden'
+      cssTextClass = ''
+      jsTextClass = 'hidden'
+    } else if (text === 'js') {
+      htmlTextClass = 'hidden'
+      cssTextClass = 'hidden'
+      jsTextClass = ''
+    }
+
     return (
       <div className="accordion">
-        <div className="title">
+        <div className="title" id="html" onClick={handleClick}>
           <h3>Hypertext Markup Language</h3>
         </div>
-        <div>
+        <div className={htmlTextClass} id="html-text">
           <p>
             HyperText Markup Language (HTML) is the standard markup language
             for creating web pages and web applications. With Cascading Style
@@ -15,10 +51,10 @@ export default class Accordion extends React.Component {
             technologies for the World Wide Web.
           </p>
         </div>
-        <div className="title">
+        <div className="title" id="css" onClick={handleClick}>
           <h3>Cascading Style Sheets</h3>
         </div>
-        <div>
+        <div className={cssTextClass} id="css-text">
           <p>
             Cascading Style Sheets (CSS) is a style sheet language used for
             describing the presentation of a document written in a markup
@@ -26,10 +62,10 @@ export default class Accordion extends React.Component {
             Wide Web, alongside HTML and JavaScript.
           </p>
         </div>
-        <div className="title">
+        <div className="title" id="js" onClick={handleClick}>
           <h3>JavaScript</h3>
         </div>
-        <div>
+        <div className={jsTextClass} id="js-text">
           <p>
             JavaScript, often abbreviated as JS, is a high-level, interpreted
             programming language that conforms to the ECMAScript specification.
