4bc7e05d190bc0da27e7f4fa8a3270b7440cfefa
Chris Gitmed <cgitmed@gmail.com>
2021-02-03 23:07:09 +0000

    Refactored component to use .map(); Tweaked styling.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,79 +1,59 @@
 import React from 'react';
 
+const topics = [
+  { title: 'HyperText Markup Language', content: 'Here is some text about HTML'},
+  { title: 'Cascading Style Sheets', content: 'Here is some text about CSS'},
+  { title: 'JavaScript', content: 'Here is some text about JS'}
+]
+
+class Content extends React.Component {
+  render() {
+    return (
+      <p>{this.props.content}</p>
+    )
+  }
+}
+
+class Title extends React.Component {
+  render() {
+    return (
+      <h1 onClick={this.props.handleClick}>{this.props.title}</h1>
+    )
+  }
+}
+
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      text: ''
-    }
+    this.state = { content: '' }
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(event) {
-    const id = event.currentTarget.id;
-    if (this.state.text === id) {
-      this.setState({text: ''});
-    } else this.setState({ text: id })
+  handleClick(index) {
+    if (this.state.content === topics[index].content) {
+      this.setState({
+        content: ''
+      });
+    } else {
+      this.setState({
+        content: topics[index].content
+      });
+    }
   }
 
   render() {
-    const { handleClick } = this;
-    const { text } = this.state;
+    const titleArray = [];
+    topics.forEach((element, index) => {
+      titleArray.push(
+        <Title handleClick={() => this.handleClick(index)} title={element.title} />
+      );
+      if (element.content === this.state.content) {
+        titleArray.push(
+          <Content content={this.state.content}/>
+        );
+      }
+    });
 
-    let htmlTextClass = 'hidden';
-    let cssTextClass = 'hidden';
-    let jsTextClass = 'hidden';
-
-    if (text === 'html') {
-      htmlTextClass = 'text-box'
-      cssTextClass = 'hidden'
-      jsTextClass = 'hidden'
-    } else if (text === 'css') {
-      htmlTextClass = 'hidden'
-      cssTextClass = 'text-box'
-      jsTextClass = 'hidden'
-    } else if (text === 'js') {
-      htmlTextClass = 'hidden'
-      cssTextClass = 'hidden'
-      jsTextClass = 'text-box'
-    }
-
-    return (
-      <div className="accordion">
-        <div className="title" id="html" onClick={handleClick}>
-          <h3>Hypertext Markup Language</h3>
-        </div>
-        <div className={htmlTextClass} id="html-text">
-          <p>
-            HyperText Markup Language (HTML) is the standard markup language
-            for creating web pages and web applications. With Cascading Style
-            Sheets(CSS) and JavaScript, it forms a triad of cornerstone
-            technologies for the World Wide Web.
-          </p>
-        </div>
-        <div className="title" id="css" onClick={handleClick}>
-          <h3>Cascading Style Sheets</h3>
-        </div>
-        <div className={cssTextClass} id="css-text">
-          <p>
-            Cascading Style Sheets (CSS) is a style sheet language used for
-            describing the presentation of a document written in a markup
-            language like HTML. CSS is a cornerstone technology of the World
-            Wide Web, alongside HTML and JavaScript.
-          </p>
-        </div>
-        <div className="title" id="js" onClick={handleClick}>
-          <h3>JavaScript</h3>
-        </div>
-        <div className={jsTextClass} id="js-text">
-          <p>
-            JavaScript, often abbreviated as JS, is a high-level, interpreted
-            programming language that conforms to the ECMAScript specification.
-            JavaScript has curly-bracket syntax, dynamic typing, prototype-based
-            object-orientation, and first-class functions.
-          </p>
-        </div>
-      </div>
-    )
+    return titleArray;
   }
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
 ReactDOM.render(
-  <Accordion />,
+  <div className="accordion">
+    <Accordion />
+  </div>,
   document.querySelector('#root')
 )
