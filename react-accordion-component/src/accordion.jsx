53051a8896f1cbb29fa544cd8660fb22d835e239
Chris Gitmed <cgitmed@gmail.com>
2021-02-05 19:48:49 +0000

    Refactored the accordion component into a hook.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const topics = [
-  { title: 'HyperText Markup Language', content: 'Here is some text about HTML'},
-  { title: 'Cascading Style Sheets', content: 'Here is some text about CSS'},
-  { title: 'JavaScript', content: 'Here is some text about JS'}
-]
+  { title: 'HyperText Markup Language', content: 'Here is some text about HTML' },
+  { title: 'Cascading Style Sheets', content: 'Here is some text about CSS' },
+  { title: 'JavaScript', content: 'Here is some text about JS' }
+];
 
 class Content extends React.Component {
   render() {
     return (
       <p>{this.props.content}</p>
-    )
+    );
   }
 }
 
@@ -18,14 +18,41 @@ class Title extends React.Component {
   render() {
     return (
       <h1 onClick={this.props.handleClick}>{this.props.title}</h1>
-    )
+    );
   }
 }
 
+export default function Accordion() {
+  const [content, setContent] = useState('');
+
+  function handleClick(index) {
+    if (content === topics[index].content) {
+      setContent('');
+    } else {
+      setContent(topics[index].content);
+    }
+  }
+
+  const titleArray = [];
+  topics.forEach((element, index) => {
+    titleArray.push(
+      <Title handleClick={() => handleClick(index)} title={element.title} />
+    );
+    if (element.content === content) {
+      titleArray.push(
+        <Content content={content}/>
+      );
+    }
+  });
+
+  return titleArray;
+}
+
+/**
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { content: '' }
+    this.state = { content: '' };
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -57,3 +84,4 @@ export default class Accordion extends React.Component {
     return titleArray;
   }
 }
+*/
