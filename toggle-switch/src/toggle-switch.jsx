23c8a1d84c8a92da697a932bb4761ad5e8f7ca00
Chris Gitmed <cgitmed@gmail.com>
2021-02-12 08:52:14 +0000

    Complete the basic functionality of the ToggleSwitch.

--- toggle-switch/src/toggle-switch.jsx
+++ toggle-switch/src/toggle-switch.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ToggleSwitch() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleClick(event) {
+    if (isActive) {
+      setIsActive(false);
+    } else setIsActive(true);
+  }
+
+  let classString = 'outer';
+  if (isActive) {
+    classString = 'outer on';
+  }
 
   return (
     <div className="border">
-      <div className="outer">
-        <div className="inner"></div>
+      <div className={classString}>
+        <div className="inner" onClick={handleClick}></div>
       </div>
     </div>
   );
