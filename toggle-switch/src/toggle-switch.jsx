26a34fad2810dd79f7b181b4937e216a58e7caea
Chris Gitmed <cgitmed@gmail.com>
2021-02-13 01:50:54 +0000

    CSS polish

--- toggle-switch/src/toggle-switch.jsx
+++ toggle-switch/src/toggle-switch.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 
-export default function ToggleSwitch() {
+export default function ToggleSwitch(props) {
   const [isActive, setIsActive] = useState(false);
 
   function handleClick(event) {
     isActive ? setIsActive(false) : setIsActive(true);
+    // props.onChange(isActive);
   }
 
   return (
     <div className="border">
-      <div className={`inner-border ${isActive ? 'on' : ''}`}>
+      <div className={`inner-border ${isActive ? 'on' : 'off'}`}>
         <div className="circle" onClick={handleClick}></div>
       </div>
     </div>
